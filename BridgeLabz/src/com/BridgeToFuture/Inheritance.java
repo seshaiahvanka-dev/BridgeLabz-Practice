0fa4322bab23bde0b140b5da684dba26b88592c6
Seshu <seshaiahvanka@gmail.com>
2026-05-22 14:31:49 +0530

    Added Inheritance.java

---
+++ BridgeLabz/src/com/BridgeToFuture/Inheritance.java
@@ -0,0 +1,30 @@
+package com.BridgeToFuture;
+
+public class Inheritance {
+
+	public static void main(String[] args) {
+		Car c = new Car();
+		System.out.println("Brand: "+c.brand);
+		c.start();
+		c.drive();
+		
+	}
+
+}
+
+class Vehicle {
+    String brand = "Honda";
+
+    void start() {
+        System.out.println("Vehicle is starting...");
+    }
+}
+
+// Child class (Derived class)
+class Car extends Vehicle {
+    int wheels = 4;
+
+    void drive() {
+        System.out.println("Car is driving with " + wheels + " wheels.");
+    }
+}
