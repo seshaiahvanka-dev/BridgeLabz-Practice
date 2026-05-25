b5e1d7c5a9801d3c02a18b04c0a0448558d52341
Seshu <seshaiahvanka@gmail.com>
2026-05-25 23:20:47 +0530

    Added StringEquality.java

---
+++ StringEquality.java
@@ -0,0 +1,21 @@
+//Write a program to check two strings are equal or not
+
+package DAY01;
+
+public class StringEquality {
+
+	public static void main(String[] args) {
+
+		String s1 = "BridgeLabz";
+		String s2 = "BridgeLabs";
+		if(s1.equals(s2))
+		{
+			System.out.println("Strings are Equal");
+		}
+		else
+		{
+			System.out.println("Strings are not equal");
+		}
+	}
+
+}
