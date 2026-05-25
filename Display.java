844f4e39ec597a06232bb958460a0a088e71b8d6
Seshu <seshaiahvanka@gmail.com>
2026-05-25 22:43:25 +0530

    Added Display.java

---
+++ Display.java
@@ -0,0 +1,10 @@
+//Write a program to display any message:
+package DAY01;
+public class Display {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Java world!");
+	}
+
+}
